b032adb1e19df48ad50d4b06b2bed777a190d8db
Форов Александр <5h9t5@list.ru>
2024-08-26 16:44:48 +0300

    feat(core/config): add  settings

---
+++ app/core/config.py
@@ -0,0 +1,28 @@
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """
+    Настройки приложения.
+
+    Attributes:
+        app_title (str): Название приложения.
+        database_url (str): URL базы данных.
+        secret (str): Секретный ключ приложения.
+        first_superuser_email (EmailStr or None, default = None): Email первого
+                                                            суперпользователя.
+        first_superuser_password (str or None, default = None): Пароль первого
+                                                            суперпользователя.
+        model_config (SettingsConfigDict): Конфигурация модели.
+    """
+    app_title: str
+    database_url: str
+    secret: str = 'secret'
+    first_superuser_email: EmailStr | None = None
+    first_superuser_password: str | None = None
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Settings()
