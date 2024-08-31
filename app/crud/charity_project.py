a0348a6eccacf29742561eb415a08a69b8e13023
Форов Александр <5h9t5@list.ru>
2024-08-31 16:03:02 +0300

    refactor(crud/charity_project): update import models

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
-from app.models.charity_project import CharityProject
+from app.models import CharityProject
 from app.schemas.charity_project import (
     CharityProjectCreate,
     CharityProjectUpdate
