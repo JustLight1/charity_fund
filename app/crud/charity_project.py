ed0c56d6c5947ce184c4b0a62e727cfd9c13530d
Форов Александр <5h9t5@list.ru>
2024-09-02 19:09:38 +0300

    feat(crud): add method create with investment

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -2,11 +2,12 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
-from app.models import CharityProject
+from app.models import CharityProject, User
 from app.schemas.charity_project import (
     CharityProjectCreate,
     CharityProjectUpdate
 )
+from app.services.investment import investment
 
 
 class CRUDCharityProject(CRUDBase[
@@ -17,6 +18,28 @@ class CRUDCharityProject(CRUDBase[
     """
     Класс для операций CRUD с моделью CharityProject.
     """
+
+    async def create(
+        self,
+        obj_in: CharityProjectCreate,
+        session: AsyncSession,
+        user: User | None = None
+    ) -> CharityProject:
+        """
+        Создает новый проект и запускает инвестиционный процесс.
+
+        Args:
+            obj_in (CharityProjectCreate): Данные для создания проекта.
+            session (AsyncSession): Асинхронная сессия SQLAlchemy.
+            user (User or None): Пользователь, создающий проект.
+
+        Returns:
+            CharityProject: Созданный проект.
+        """
+        project = await super().create(obj_in, session, user)
+        await investment(session)
+        await session.refresh(project)
+        return project
 
     async def get_project_id_by_name(
             self,
--- app/crud/donation.py
+++ app/crud/donation.py
@@ -1,19 +1,44 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from pydantic import BaseModel
+
 from app.crud.base import CRUDBase
 from app.models import Donation, User
-from app.schemas.donation import DonationCreate, DonationUpdate
+from app.schemas.donation import DonationCreate
+from app.services.investment import investment
 
 
 class CRUDDonation(CRUDBase[
     Donation,
     DonationCreate,
-    DonationUpdate
+    BaseModel
 ]):
     """
     Класс для операций CRUD с моделью Donation.
     """
+
+    async def create(
+        self,
+        obj_in: DonationCreate,
+        session: AsyncSession,
+        user: User | None = None
+    ) -> Donation:
+        """
+        Создает новое пожертвование и запускает инвестиционный процесс.
+
+        Args:
+            obj_in (DonationCreate): Данные для создания пожертвования.
+            session (AsyncSession): Асинхронная сессия SQLAlchemy.
+            user (User or None): Пользователь, создающий пожертвование.
+
+        Returns:
+            Donation: Созданное пожертвование.
+        """
+        donation = await super().create(obj_in, session, user)
+        await investment(session)
+        await session.refresh(donation)
+        return donation
 
     async def get_by_user(
         self,
