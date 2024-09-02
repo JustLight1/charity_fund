0d781c06a6abf74f736c6eab532c92e6e0433341
Форов Александр <5h9t5@list.ru>
2024-09-02 19:08:03 +0300

    feat(api/routers): add routers

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import user_router
+from app.api.endpoints import (charity_project_router, user_router,
+                               donation_router)
 
 
 main_router = APIRouter()
 
+main_router.include_router(charity_project_router)
+main_router.include_router(donation_router)
 main_router.include_router(user_router)
