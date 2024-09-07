9654977ee6faad4a69b2f2eb126e85a77eade9a4
Форов Александр <5h9t5@list.ru>
2024-09-07 15:35:21 +0300

    feat(api/routers): add google_api_router

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import (charity_project_router, user_router,
-                               donation_router)
+                               donation_router, google_api_router)
 
 
 main_router = APIRouter()
@@ -9,3 +9,4 @@ main_router = APIRouter()
 main_router.include_router(charity_project_router)
 main_router.include_router(donation_router)
 main_router.include_router(user_router)
+main_router.include_router(google_api_router)
